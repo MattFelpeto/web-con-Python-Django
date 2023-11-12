962ea0290d6c540b1e1ebc670a97e7b34afea5c3
MattFelpeto <matias.felpeto@gmail.com>
2023-11-12 13:02:21 -0300

    cambios pruebas

--- tablakarting/wsgi.py
+++ tablakarting/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tablakarting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tablakarting.settings', 'miembros.settings')
 
 application = get_wsgi_application()
