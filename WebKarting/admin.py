5d526a2ae88f707cdae8a2a56486d4ee8c6d864a
MattFelpeto <matias.felpeto@gmail.com>
2023-11-12 21:40:51 -0300

    cambios en models

--- WebKarting/admin.py
+++ WebKarting/admin.py
@@ -6,8 +6,8 @@ from .models import Piloto_Jordy
 from .models import Piloto_Pato
 from .models import Piloto_Cristian
 from .models import Piloto_Martincho
-from .models import Piloto_Pedro
-from .models import Piloto_Agustin
+from .models import Piloto_Guada
+from .models import Piloto_Yoa
 from .models import Fechas_Carreras
 
 admin.site.register(Piloto_Matias)
@@ -16,6 +16,6 @@ admin.site.register(Piloto_Jordy)
 admin.site.register(Piloto_Pato)
 admin.site.register(Piloto_Cristian)
 admin.site.register(Piloto_Martincho)
-admin.site.register(Piloto_Pedro)
-admin.site.register(Piloto_Agustin)
+admin.site.register(Piloto_Guada)
+admin.site.register(Piloto_Yoa)
 admin.site.register(Fechas_Carreras)
