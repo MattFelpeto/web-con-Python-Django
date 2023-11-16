0888d0abb88b80e1f33a2cb32799a5eca533e409
MattFelpeto <matias.felpeto@gmail.com>
2023-11-15 23:18:08 -0300

    agregado de regla de puntos again again and again again

--- WebKarting/urls.py
+++ WebKarting/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"), 
     path('register_user', views.register_user, name="register_user"),
-    path('info_puntos', views.puntuacion, name='como_se_puntua')    
+    path('info_puntaje', views.puntuacion, name='como_se_puntua')    
     ]
